457903cbca36b62acec458df8bc352f2c6ab01d6
adityx-reposit <adityayadav2022@vitbhopal.ac.in>
2025-05-20 15:47:06 +0530

    pushed

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ const user = new Client(
 );
 user.connect()
 
+const address=new Client("postgresql://neondb_owner:npg_4mbvoq3RBQhf@ep-empty-brook-a5b1eu65-pooler.us-east-2.aws.neon.tech/neondb?sslmode=true")
+address.connect()
 
 
 app.post("/signup",async(req,res)=>{
@@ -16,9 +18,18 @@ app.post("/signup",async(req,res)=>{
     const password=req.body.password;
 
 
+
+    
+    const city=req.body.city
+    const country=req.body.country
+    const street=req.body.street
+    const pincode=req.body.pincode
+
     const response=await user.query(`INSERT into users(username,email,password) VALUES('${username}','${email}','${password}')`)
      console.log(response);
      
+
+     const adresssInsertQuery=await address.query(`INSERT INTO address(city,country,street,pincode) VALUES('${city}','${country}', '${street}','${pincode}')`)
    
     res.json({
         
